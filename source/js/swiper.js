6bbbb87b9e71ce56787f6666b54842abef7d2b99
Evgeniy <nikolaev_zheka-zik@bk.ru>
2022-06-23 17:22:56 +0300

    добавляет секцию преимуществ и кусочек фильтра

---
+++ source/js/swiper.js
@@ -0,0 +1,14 @@
+import Swiper from 'swiper/bundle'
+import 'swiper/css/bundle';
+
+export default swiper = new Swiper('.swiper', {
+  direction: 'horizontal',
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
