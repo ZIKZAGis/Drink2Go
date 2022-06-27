157c4a1b6b4b78eb4095a2c360ea0ee4d54f413c
Evgeniy <nikolaev_zheka-zik@bk.ru>
2022-06-27 16:46:51 +0300

    Оживляет слайдер и добавляет интерактивную карту

--- source/js/script.js
+++ source/js/script.js
@@ -13,3 +13,37 @@ navToggle.addEventListener('click', () => {
   }
 })
 
+// Swiper
+
+const swiper = new Swiper('.swiper__wrapper', {
+  direction: 'horizontal',
+  loop: true,
+
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
+
+// Map
+
+const map = L.map('map',{scrollWheelZoom:true}).setView([59.968435975884354, 30.317528294049186], 20);
+
+const basemaps = {
+  StreetView: L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'}),
+};
+
+L.control.layers(basemaps).addTo(map);
+
+basemaps.StreetView.addTo(map);
+
+const mark = L.icon({iconUrl: '../img/mappin.svg', iconSize: [38, 50],});
+
+const marker = L.marker([59.968435975884354, 30.317528294049186], {icon: mark})
+  .bindPopup('Drink2Go - Интернет магазин кофейных напитков').addTo(map);
+
